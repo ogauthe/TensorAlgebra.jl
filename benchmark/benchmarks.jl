2c46a8ef5e19656713ef57b6cc3b882f2eaec872
Lukas Devos <ldevos98@gmail.com>
2025-03-13 15:29:43 -0400

    Add contraction benchmarks (#38)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,6 +2,25 @@ using TensorAlgebra
 using BenchmarkTools
 
 SUITE = BenchmarkGroup()
-SUITE["rand"] = @benchmarkable rand(10)
 
-# Write your benchmarks here.
+const CONTRACTIONS_PATH = joinpath(@__DIR__, "benchmark_specs", "randomTCs.dat")
+
+include("contractions.jl")
+
+# Contraction benchmarks
+# ----------------------
+contraction_suite = SUITE["contractions"] = BenchmarkGroup()
+
+Ts = (Float64, ComplexF64)
+algs = (TensorAlgebra.Matricize(),)
+
+for alg in algs
+  alg_suite = contraction_suite[alg] = BenchmarkGroup()
+  for T in Ts
+    alg_suite[T] = BenchmarkGroup()
+
+    for (i, line) in enumerate(eachline(CONTRACTIONS_PATH))
+      alg_suite[T][i] = generate_contract_benchmark(line; T, alg)
+    end
+  end
+end
